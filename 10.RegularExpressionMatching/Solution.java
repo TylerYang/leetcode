ce3684bb58cebe5fbc3af24162151b33300521ed
Tyler Yang <tyler.z.yang@gmail.com>
2015-06-14 21:49:03 +0800

    Add Regular Expression Matching Recursive Solution

---
+++ 10.RegularExpressionMatching/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public boolean isMatch(String s, String p){
+        if(p.length() == 0){
+            if(s.length() == 0) return true;
+            return false;
+        }
+        if(p.length() == 1){
+            if(s.length() == 1 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')){
+                return true;
+            }
+            return false;
+        }
+        
+        if(p.charAt(1) != '*'){
+            if(s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')){
+                return isMatch(s.substring(1), p.substring(1)); 
+            }
+            return false;
+        } else {
+            String subP = p.substring(2);
+            if(isMatch(s, subP)){
+                return true;
+            } else {
+                char c = p.charAt(0);
+                for(int i = 0; i < s.length() && (s.charAt(i) == c || c == '.'); i++){
+                    if(isMatch(s.substring(i + 1), subP)) return true;
+                }
+                return false;
+            }
+    
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,16 +5,16 @@ Tyler's leetcode respository.
 
 Here is the question list:
 (Difficulty Distribution: 1-5 Hard)
-* 1. Two Sum (2)
-* 2. Add Two Numbers (3)
-* 3. Logest String Without Repeating Characters (3)
-* 4. Median of Two Sorted Arrays (5) *
-* 5. Longest Palindromic Substring (3) (Brute force, Dynamic Programming) 
-* 6. ZigZag Conversion (3) 
-* 7. Reverse Integer(2) 
-* 8. String to Integer(2) 
-* 9. Palidrome Number(2) (Reverse, Generic)
-
+* 1.   Two Sum (2)
+* 2.   Add Two Numbers (3)
+* 3.   Logest String Without Repeating Characters (3)
+* 4.   Median of Two Sorted Arrays (5) *
+* 5.   Longest Palindromic Substring (3) (Brute force, Dynamic Programming) 
+* 6.   ZigZag Conversion (3) 
+* 7.   Reverse Integer(2) 
+* 8.   String to Integer(2) 
+* 9.   Palidrome Number(2) (Reverse, Generic)
+* 10.  Regular Expression Matching(5) (Recursive)
 
 
 
