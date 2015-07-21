27dbb9b0ac930935c7788427368b66008b643f43
Tyler Yang <tyler.z.yang@gmail.com>
2015-07-21 11:08:54 +0800

    Fix bug

--- 99.RecoverBinarySearchTree/Solution2.java
+++ 99.RecoverBinarySearchTree/Solution2.java
@@ -2,13 +2,14 @@ public class Solution2 {
     public void recoverTree(TreeNode root) {
         //Morris Traversal
         if(root == null) return;
-        TreeNode n1, n2, prevNode = root.left == null ? root : null;
+        TreeNode n1 = null, n2 = null, prevNode = root.left == null ? root : null;
         while(root != null) {
             if(root.left == null) {
                 if(prevNode != null && prevNode.val > root.val) {
                     if(n1 == null) n1 = prevNode;
                     n2 = root;
                 }
+                prevNode = root;
                 root = root.right;
             } else {
                 TreeNode node = root.left;
@@ -22,6 +23,7 @@ public class Solution2 {
                         n2 = root;
                     }
                     node.right = null;
+                    prevNode = root;
                     root = root.right;
                 }
             }
