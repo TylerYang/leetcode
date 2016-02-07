1a176cab0feb3d069a45c178c8f7bc7d7b94ef88
Tyler <tyler.z.yang@gmail.com>
2016-02-07 11:27:34 +0800

    Implement 329 solution in a more elegant way

--- 329.LongestIncreasingPathInAMatrix/Solution.java
+++ 329.LongestIncreasingPathInAMatrix/Solution.java
@@ -1,56 +1,30 @@
 public class Solution {
     int[][] paths;
     int count;
+    int[] v = {1, 0, -1, 0};
+    int[] h = {0, 1, 0, -1};
+
     public int longestIncreasingPath(int[][] matrix) {
       if(matrix.length == 0) return 0;
       if(matrix[0].length == 0) return 0;
       count = 1;
       paths = new int[matrix.length][matrix[0].length];
-      System.out.println("wtf:" + matrix[0].length);
       for(int i = 0; i < matrix.length; i++) {
         for(int j = 0; j < matrix[0].length; j++) {
-          int path = 1;
-          if(i != 0 && matrix[i - 1][j] > matrix[i][j]) {
-            int tmp = calPath(matrix, i - 1, j);
-            if(tmp >= path) path = tmp + 1;
-          }
-          if(i != matrix.length - 1 && matrix[i + 1][j] > matrix[i][j]) {
-            int tmp = calPath(matrix, i + 1, j);
-            if(tmp >= path) path = tmp + 1;
-          }
-          if(j != 0 && matrix[i][j - 1] > matrix[i][j]) {
-            int tmp = calPath(matrix, i, j - 1);
-            if(tmp >= path) path = tmp + 1;
-          }
-          if(j != matrix[0].length - 1 && matrix[i][j + 1] > matrix[i][j]) {
-            int tmp = calPath(matrix, i, j + 1);
-            if(tmp >= path) path = tmp + 1;
-          }
-          paths[i][j] = path;
-          if(path > count) count = path;
+          dfs(matrix, i, j);
         }
       }
       return count;
     }
-    public int calPath(int[][] matrix, int i, int j) {
-      System.out.println("i:" + i + " ,j:" + j);
+    public int dfs(int[][] matrix, int i, int j) {
       if(paths[i][j] != 0) return paths[i][j];
       int path = 1;
-      if(i != 0 && matrix[i - 1][j] > matrix[i][j]) {
-        int tmp = calPath(matrix, i - 1, j);
-        if(tmp >= path) path = tmp + 1;
-      }
-      if(i != matrix.length - 1 && matrix[i + 1][j] > matrix[i][j]) {
-        int tmp = calPath(matrix, i + 1, j);
-        if(tmp >= path) path = tmp + 1;
-      }
-      if(j != 0 && matrix[i][j - 1] > matrix[i][j]) {
-        int tmp = calPath(matrix, i, j - 1);
-        if(tmp >= path) path = tmp + 1;
-      }
-      if(j != matrix[0].length - 1 && matrix[i][j + 1] > matrix[i][j]) {
-        int tmp = calPath(matrix, i, j + 1);
-        if(tmp >= path) path = tmp + 1;
+      for(int k = 0; k < 4; k++) {
+        int x = i + v[k], y = j + h[k];
+        if(x >= 0 && y >= 0 && x < matrix.length && y < matrix[0].length && matrix[x][y] > matrix[i][j]) {
+          int tmp = dfs(matrix, x, y);
+          if(tmp >= path) path = tmp + 1;
+        }
       }
       paths[i][j] = path;
       if(path > count) count = path;
