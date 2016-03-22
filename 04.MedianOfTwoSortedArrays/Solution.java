c8d639cc23a38f1fb1b851937bed661f07652627
Tyler <tyler.z.yang@gmail.com>
2016-03-22 13:37:11 +0800

    Update solution

--- 04.MedianOfTwoSortedArrays/Solution.java
+++ 04.MedianOfTwoSortedArrays/Solution.java
@@ -5,17 +5,17 @@ public class Solution{
         if(n % 2 == 1){
             return findKth(nums1, 0, nums2, 0, n/2 + 1);
         } else {
-            return (findKth(nums1, 0, nums2, 0, n/2) + findKth(nums1, 0, nums2, 0, n/2 + 1))/2.0; 
+            return (findKth(nums1, 0, nums2, 0, n/2) + findKth(nums1, 0, nums2, 0, n/2 + 1))/2.0;
         }
     }
     private int findKth(int[] A, int startA, int[] B, int startB, int k){
         if(startA >= A.length) return B[startB + k - 1];
         if(startB >= B.length) return A[startA + k - 1];
         if(k == 1) return A[startA] < B[startB] ? A[startA] : B[startB];
-        
+
         int currA = startA + k/2 - 1 < A.length ? A[startA + k/2 - 1]:Integer.MAX_VALUE;
         int currB = startB + k/2 - 1 < B.length ? B[startB + k/2 - 1]:Integer.MAX_VALUE;
-        
+
         if(currA < currB){
             return findKth(A, startA + k/2, B, startB, k - k/2);
         } else {
--- 30.SubstringWithConcatenationOfAllWords/Solution.java
+++ 30.SubstringWithConcatenationOfAllWords/Solution.java
@@ -1,63 +1,53 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Iterator;
-
 public class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> rList = new ArrayList<Integer>();
-        if(s.length() == 0 || words.length == 0 || words[0].length() == 0) return  rList;
-        int sLen = s.length(), wLen = words[0].length();
-        int wsLen = wLen * words.length;
-        
-        Map<String, Integer> wordsMap = new HashMap<String, Integer>();
-        Map<String, Integer> foundMap = new HashMap<String, Integer>();
-        for(int i = 0; i < words.length; i++) {
-            if(wordsMap.containsKey(words[i])){
-                wordsMap.put(words[i], wordsMap.get(words[i]) + 1);
+        List<Integer> re = new ArrayList<Integer>();
+        if (words.length == 0 || words.length * words[0].length() > s.length()) return re;
+        int count = 0, sLen = s.length(), wordsLen = words.length, wLen = words[0].length();
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        Map<String, Integer> currMap = new HashMap<String, Integer>();
+        for (String w: words) {
+            if(map.containsKey(w)) {
+                map.put(w, map.get(w) + 1);
             } else {
-                wordsMap.put(words[i], 1);
+                map.put(w, 1);
             }
-            foundMap.put(words[i], 0);
-        }
-          
-        for(int i = 0; i < wLen; i++) {
-            int start = i, end = start + wLen;
-            while(end <= sLen) {
-                String str = s.substring(end - wLen, end);
-                if(wordsMap.containsKey(str)) {
-                    if(wordsMap.get(str) == foundMap.get(str)) {
-                        while(wordsMap.get(str) == foundMap.get(str)) {
-                            String tmpStr = s.substring(start, start + wLen);
-                            foundMap.put(tmpStr, foundMap.get(tmpStr) - 1);
-                            start += wLen;
-                        }     
-                    } else {
-                        if(end - start == wsLen) {
-                            rList.add(start);
-                            String tmpStr = s.substring(start, start + wLen); 
-                            foundMap.put(tmpStr, foundMap.get(tmpStr) - 1);
-                            start += wLen;
-                        } 
-                    }
-                    foundMap.put(str, foundMap.get(str) + 1);
-                } else {
-                    start = end;
-                    clearMap(foundMap);
-                }
-                end += wLen;
-            }          
-            clearMap(foundMap);
-        }
-        return rList;
-    }
-    private void clearMap(Map<String, Integer> foundMap) {
-        Iterator<Map.Entry<String, Integer>> entries = foundMap.entrySet().iterator();
-        while(entries.hasNext()) {
-            Map.Entry<String, Integer> entry = entries.next();
-            foundMap.put(entry.getKey(), 0);
         }
         
+        for (int i = 0; i < wLen; i++) {
+            for (int l = i, r = i; r + wLen <= sLen; r += wLen) {
+                String tmp = s.substring(r, r + wLen);
+                if (map.containsKey(tmp)) {
+                    if (currMap.containsKey(tmp)) {
+                        currMap.put(tmp, currMap.get(tmp) + 1);
+                        
+                        while (currMap.get(tmp) > map.get(tmp)) {
+                            String leftTmp = s.substring(l, l + wLen);
+                            currMap.put(leftTmp, currMap.get(leftTmp) - 1);
+                            l += wLen;
+                            count--;
+                        }
+                    } else {
+                        currMap.put(tmp, 1);
+                    }
+                    count++;
+                    if (count == wordsLen) {
+                        re.add(l);
+                        
+                        l += wLen;
+                        String leftTmp = s.substring(l - wLen, l);
+                        currMap.put(leftTmp, currMap.get(leftTmp) - 1);
+                        
+                        count--;
+                    }
+                } else {
+                    l = r + wLen;
+                    count = 0;
+                    currMap.clear();
+                }
+            }
+            currMap.clear();
+            count = 0;
+        }
+        return re;
     }
 }
