f13a30f4fe8a655e262d6c74b7ca0594975ac3c9
Tyler <tyler.z.yang@gmail.com>
2016-04-01 20:27:20 +0800

    Update edit distance solution

--- 72.EditDistance/Solution.java
+++ 72.EditDistance/Solution.java
@@ -1,29 +1,23 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
-        if(word1.length() == 0) return word2.length();
-        if(word2.length() == 0) return word1.length();
+        if (word1.length() == 0) return word2.length();
+        if (word2.length() == 0) return word1.length();
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
-        for(int i = 0; i < dp.length; i++) dp[i][0] = i;
-        for(int i = 0; i < dp[0].length; i++) dp[0][i] = i;
+        for (int i = 0; i <= word1.length(); i++) dp[i][0] = i;
+        for (int i = 1; i <= word2.length(); i++) dp[0][i] = i;
         
-        for(int i = 0; i < word1.length(); i++) {
-            char c1 = word1.charAt(i);
-            for(int j = 0; j < word2.length(); j++) {
-                char c2 = word2.charAt(j);
-                if(c1 == c2) {
-                    dp[i + 1][j + 1] = dp[i][j];
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    int delete = dp[i][j + 1];
-                    int replace = dp[i][j];
-                    int insert = dp[i + 1][j];
-                    dp[i + 1][j + 1] = min(delete, replace, insert) + 1;
+                    int insertCost = dp[i - 1][j];
+                    int replaceCost = dp[i - 1][j - 1];
+                    int deleteCost = dp[i][j - 1];
+                    dp[i][j] = Math.min(Math.min(insertCost, replaceCost), deleteCost) + 1;
                 }
             }
         }
         return dp[word1.length()][word2.length()];
-    }
-    private int min(int a, int b, int c) {
-        int r = a < b ? a : b;
-        return r < c ? r : c;
-    }
+     }
 }
