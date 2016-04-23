cf501af0f1bd918f85998361a90679f11de6b795
Tyler <tyler.z.yang@gmail.com>
2016-04-23 11:43:13 +0800

    Update interleaving string solution

--- 97.InterleavingString/Solution.java
+++ 97.InterleavingString/Solution.java
@@ -1,20 +1,24 @@
 public class Solution {
-    public boolean isInterleaving(String s1, String s2, String s3) {
-        if(s1.length() == 0) return s2.equals(s3);
-        if(s2.length() == 0) return s1.equals(s3);
-        if(s1.length() + s2.length() != s3.length()) return false;
-        
-        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 2];
-        for(int i = 0; i <= s1.length(); i++) {
-            for(int j = 0; j <= s2.length(); j++) {
-                if(i == 0 && j == 0) {
-                    dp[i][j] = true;
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;
+        if (s1.length() == 0) return s2.equals(s3);
+        if (s2.length() == 0) return s1.equals(s3);
+
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[0][0] = true;
                 } else {
-                    dp[i][j] = (i > 0 && dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (j > 0 && dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1)); 
+                    char currC = s3.charAt(i + j - 1);
+                    if (i > 0 && dp[i - 1][j] == true) {
+                        dp[i][j] = s1.charAt(i - 1) == currC;
+                    } else if (j > 0 && dp[i][j - 1] == true) {
+                        dp[i][j] = s2.charAt(j - 1) == currC;
+                    }
                 }
             }
         }
         return dp[s1.length()][s2.length()];
-            
     }
 }
