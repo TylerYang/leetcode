e293187446d86b91ec59fd0b49cf7799836b68b5
Tyler <tyler.z.yang@gmail.com>
2016-04-26 13:14:14 +0800

    Update recover binary search tree solution

--- 99.RecoverBinarySearchTree/Solution.java
+++ 99.RecoverBinarySearchTree/Solution.java
@@ -1,27 +1,49 @@
-import java.util.*;
 public class Solution {
-	public void recoverTree(TreeNode root) {
-		if(root == null) return;
-		Stack<TreeNode> rStack = new Stack<TreeNode>();
-		TreeNode n1 = null, n2 = null, prevNode = root.left == null ? root : null;
-		while(root != null) {
-			rStack.push(root);
-			root = root.left;
-		}
-		while(rStack.empty() == false) {
-			TreeNode curr = rStack.pop();
-			TreeNode node = curr.right;
-			while(node != null) {
-				rStack.push(node);
-				node = node.left;
-			}
-			if(n1 == null && rStack.empty() == false && rStack.peek().val < curr.val) n1 = curr;
-			if(prevNode != null && prevNode.val > curr.val) n2 = curr;
-			prevNode = curr;
-		}
-		int tmp = n1.val;
-		n1.val = n2.val;
-		n2.val = tmp;
-		return;
-	}
+    public void recoverTree(TreeNode root) {
+        TreeNode firstNode = null, secondNode = null;
+        TreeNode curr = root;
+
+        //preorder morris traversal
+        TreeNode prevNode = null;
+
+        while (curr != null) {
+            if (curr.left == null) {
+                if (prevNode != null && prevNode.val > curr.val) {
+                    if (firstNode == null) {
+                        firstNode = prevNode;
+                    }
+                    secondNode = curr;
+                }
+                prevNode = curr;
+                curr = curr.right;
+            } else {
+                TreeNode nextNode = curr.left;
+                while (nextNode.right != null && nextNode.right != curr) {
+                    nextNode = nextNode.right;
+                }
+                if (nextNode.right == null) {
+                    nextNode.right = curr;
+                    curr = curr.left;
+                } else {
+                    nextNode.right = null;
+                    if (prevNode != null && prevNode.val > curr.val) {
+                        if (firstNode == null) {
+                            firstNode = prevNode;
+                        }
+                        secondNode = curr;
+                    }
+                    prevNode = curr;
+                    curr = curr.right;
+                }
+            }
+        }
+        if (firstNode != null) {
+            swapValue(firstNode, secondNode);
+        }
+    }
+    private void swapValue(TreeNode n1, TreeNode n2) {
+        int val = n1.val;
+        n1.val = n2.val;
+        n2.val = val;
+    }
 }
