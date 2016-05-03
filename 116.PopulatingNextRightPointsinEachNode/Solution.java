7a0a516b21a7c69937b94b5a4a0b88d79e64bce5
Tyler <tyler.z.yang@gmail.com>
2016-05-03 13:18:48 +0800

    Update populating next right points in each node solution

--- 116.PopulatingNextRightPointsinEachNode/Solution.java
+++ 116.PopulatingNextRightPointsinEachNode/Solution.java
@@ -1,26 +1,21 @@
 public class Solution {
     public void connect(TreeLinkNode root) {
-        if(root == null) return;
-        TreeLinkNode p = root;
-        TreeLinkNode first = null;
-        while(p) {
-            if(first == null) {
-                first = p.left;
+            TreeLinkNode curr = root;
+        TreeLinkNode prev = null;
+        while (curr != null) {
+            TreeLinkNode tmp = curr;
+            prev = null;
+
+            while (tmp != null && tmp.left != null) {
+                tmp.left.next = tmp.right;
+                if (prev != null) {
+                    prev.next = tmp.left;
+                }
+                prev = tmp.right;
+                tmp = tmp.next;
             }
-            if(p.left != null) {
-                p.left.next = p.right;
-            }
-            if(p.right != null && p.next != null) {
-                p.right.next = p.next.left;
-            }
-            if(p.next == null) {
-                p = first;
-                first = null;
-            } else {
-                p = p.next;
-            }
+            curr = curr.left;
         }
         return;
-
     }
 }
