41d9281df168bd2d5299ad1865ba21c9af6cb5b2
Tyler <tyler.z.yang@gmail.com>
2016-05-17 13:00:57 +0800

    Update solution

--- 123.BestTimeToBuyAndSellStockIII/Solution.java
+++ 123.BestTimeToBuyAndSellStockIII/Solution.java
@@ -1,37 +1,28 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length <= 1) return 0;
-        int[] profits = new int[prices.length];
-        int profit = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < prices.length; i++) {
-            if(min > prices[i]) min = prices[i];
-            if(max < prices[i]) {
-                max = prices[i];
-                profit = max - min;
-            } else {
-                if(prices[i] - min > profit) profit = prices[i] - min;
-            }
-            profits[i] = profit;
-        }
-        
-        int maxProfit = 0;
-        max = Integer.MIN_VALUE;
-        min = Integer.MAX_VALUE;
-        profit = 0;
-       
-        for(int i = prices.length - 1; i >= 0; i--) {
-            if(max < prices[i]) max = prices[i];
-            if(min > prices[i]) {
-                min = prices[i];
-                profit = max - min;
-            } else {
-                if(max - prices[i] > profit) profit = max - prices[i];
-            }
-            if(profit + profits[i] > maxProfit) maxProfit = profit + profits[i];
-        }
-        return maxProfit; 
+        if (prices.length == 0) return 0;
+        int[] dp = new int[prices.length];
 
+        int tmp = prices[prices.length - 1], max = 0;
+        for (int i = prices.length - 2; i >= 0; i--) {
+            if (tmp - prices[i] > max) {
+                dp[i] = tmp - prices[i];
+                max = dp[i];
+            } else {
+                dp[i] = max;
+            }
+            if (prices[i] > tmp) tmp = prices[i];
+        }
+
+        tmp = prices[0];
+        for (int i = 1; i < prices.length - 1; i++) {
+            int profit = prices[i] - tmp;
+
+            if (profit + dp[i + 1] > max) {
+                max = profit + dp[i + 1];
+            }
+            if (prices[i] < tmp) tmp = prices[i];
+        }
+        return max;
     }
-    
 }
