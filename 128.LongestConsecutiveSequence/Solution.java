7fd8237b69b204587d740b4a8aa2a2c029f59a59
Tyler <tyler.z.yang@gmail.com>
2016-06-11 19:25:14 +0800

    Update lcs solution

--- 128.LongestConsecutiveSequence/Solution.java
+++ 128.LongestConsecutiveSequence/Solution.java
@@ -1,28 +1,19 @@
 public class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> numSet = new HashSet<Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         int max = 0;
-        for(int i = 0; i < nums.length; i++) {
-            numSet.add(nums[i]);
-        }
-        
-        for(int i = 0; i < nums.length; i++) {
-            if(numSet.contains(nums[i])) {
-                int count = 1;
-                int left = nums[i] - 1, right = nums[i] + 1;
-                while(numSet.contains(left)) {
-                    count++;
-                    numSet.remove(left);
-                    left--;
-                }
-                
-                while(numSet.contains(right)) {
-                    count++;
-                    numSet.remove(right);
-                    right++;
-                }
-                if(count > max) max = count;
-            }
+        for (int n: nums) {
+            if (map.containsKey(n)) continue;
+            int left = map.containsKey(n - 1) ? map.get(n - 1) : 0;
+            int right = map.containsKey(n + 1) ? map.get(n + 1) : 0;
+
+            int sum = left + right + 1;
+            map.put(n, sum);
+
+            max = Math.max(max, sum);
+
+            map.put(n - left, sum);
+            map.put(n + right, sum);
         }
         return max;
     }
